dd7b381856a81f3d38f3725cb6ed98b3677be3ef
Rene Cocom <fasnimo@gmail.com>
2019-06-21 00:57:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(names)
+  names.map do |speaker|
+    badge_maker(speaker)
+  end
+end
+
+def assign_rooms(name)
+  name.each_with_index do |string, index|
+      return "Hello, #{string}! You'll be assigned to room #{index}!"
+  end
+end
