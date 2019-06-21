5bc7174fcdc55a463074da551eb7673fb3db1da9
Rene Cocom <fasnimo@gmail.com>
2019-06-21 00:59:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,6 @@ def assign_rooms(name)
   new = []
   name.each_with_index do |string, index|
       "Hello, #{string}! You'll be assigned to room #{index}!"
-        return new
+        name
   end
 end
